fd600e01433c45d41efb36d79358ff36ed830008
chr1swill <alexanderwilliams20003@gmail.com>
2024-04-26 20:00:57 -0400

    feat: made basic ui work - need to fix bug where no matter what an error is throw on startup

--- main.js
+++ main.js
@@ -1,71 +1,168 @@
-class TimerController {
-	/**
-	 * @type{number}
-	 */
-	expected = 0;
+(function () {
+	class TimerController {
+		/**
+		 * @type{number}
+		 */
+		expected = 0;
 
-	/**
-	 * @type{number}
-	 */
-	timeout = 0;
+		/**
+		 * @type{number}
+		 */
+		timeout = 0;
 
-	/**
-	 * @type{number}
-	 */
-	interval = 0;
+		/**
+		 * @type{number}
+		 */
+		interval = 0;
 
-	/**
-	 * @param{Function} workFunc
-	 * @param{number} interval
-	 * @param{Function} errorFunc
-	 */
-	constructor(workFunc, interval, errorFunc) {
-		this.workFunc = workFunc;
-		this.interval = interval;
-		this.errorFunc = errorFunc;
-		this.step = this.step.bind(this);
-	}
+		/**
+		 * @type{boolean}
+		 */
+		#isTimerRunning = false;
 
-	/*
-	 * @public
-	 */
-	start() {
-		const now = Date.now();
-		this.expected = now + this.interval;
-		this.timeout = window.setTimeout(() => this.step(), this.interval);
-	}
-
-	/*
-	 * @public
-	 */
-	stop() {
-		window.clearTimeout(this.timeout);
-	}
-
-	/*
-	 * @private
-	 */
-	step() {
-		const now = Date.now();
-		const drift = this.expected - now;
-		if (drift > this.interval) {
-			if (this.errorFunc) this.errorFunc();
+		/**
+		 * @param{Function} workFunc
+		 * @param{number} interval
+		 * @param{Function} errorFunc
+		 */
+		constructor(workFunc, interval, errorFunc) {
+			this.workFunc = workFunc;
+			this.interval = interval;
+			this.errorFunc = errorFunc;
+			this.step = this.#step.bind(this);
 		}
-		this.workFunc();
-		this.expected += this.interval;
-		this.timeout = window.setTimeout(this.step, Math.max(0, this.interval));
+
+		start() {
+			if (this.#isTimerRunning === true) {
+				console.error(
+					"You can not start a new timer, a timer is currently running",
+				);
+				return;
+			}
+			const now = Date.now();
+			this.expected = now + this.interval;
+			this.timeout = window.setTimeout(() => this.step(), this.interval);
+			this.#isTimerRunning = true;
+		}
+
+		stop() {
+			window.clearTimeout(this.timeout);
+			this.#isTimerRunning = false;
+		}
+
+		#step() {
+			const now = Date.now();
+			const drift = this.expected - now;
+			if (drift > this.interval) {
+				if (this.errorFunc) this.errorFunc();
+			}
+			this.workFunc();
+			this.expected += this.interval;
+			this.timeout = window.setTimeout(this.step, Math.max(0, this.interval));
+			if (timeToCountDownFrom === 0) {
+				this.#reset();
+				return;
+			}
+		}
+
+		timerRunning() {
+			return this.#isTimerRunning;
+		}
+
+		#reset() {
+			this.stop();
+			this.expected = 0;
+			this.interval = 0;
+		}
 	}
-}
 
-let justSomeNumber = 0;
+	const timeSelector = /**@type{HTMLInputElement|null}*/ (
+		document.getElementById("selectTime")
+	);
+	const startButton = /**@type{HTMLButtonElement|null}*/ (
+		document.getElementById("startTimer")
+	);
+	const pauseButton = /**@type{HTMLButtonElement|null}*/ (
+		document.getElementById("pauseTimer")
+	);
+	const stopButton = /**@type{HTMLButtonElement|null}*/ (
+		document.getElementById("stopTimer")
+	);
+	const displayTime = /**@type{HTMLParagraphElement|null}*/ (
+		document.getElementById("displayTime")
+	);
 
-const doWork = function () {
-	console.log(++justSomeNumber);
-};
+	if (
+		timeSelector === null ||
+		startButton === null ||
+		pauseButton === null ||
+		stopButton === null ||
+		displayTime === null
+	) {
+		console.error("Could not find the ui control elements needed");
+		return;
+	}
 
-const doError = function () {
-	console.warn("The drift exceeded the interval.");
-};
+	/**@type{number}*/
+	let timeToCountDownFrom;
 
-const ticker = new TimerController(doWork, 1000, doError);
-ticker.start();
+	const doWork = function () {
+		timeToCountDownFrom--;
+		console.log("Fired with value: ", timeToCountDownFrom);
+		displayTime.textContent = timeToCountDownFrom.toString();
+	};
+
+	const doError = function () {
+		console.warn("The drift exceeded the interval.");
+	};
+
+	/**@type{TimerController|undefined}*/
+	let ticker = new TimerController(doWork, 1000, doError);
+
+	startButton.onclick = function () {
+		if (timeSelector.value.trim() === "") {
+			console.error("You have not selected a time");
+			return;
+		}
+		if (ticker.timerRunning() === true) {
+			console.log(
+				"A timer is currently running, if you would like to start a new now stop the only one first",
+			);
+			return;
+		}
+
+		if (ticker.timerRunning() === false && displayTime.textContent !== "0") {
+			ticker.start();
+			return;
+		}
+
+		if (displayTime.textContent === "0" || displayTime.textContent === null) {
+			timeToCountDownFrom = timeSelector.valueAsNumber;
+			displayTime.textContent = timeSelector.value;
+			ticker.start();
+		} else {
+			timeToCountDownFrom = parseInt(displayTime.textContent);
+			ticker.start();
+		}
+	};
+
+	pauseButton.onclick = function () {
+		if (timeSelector.value.trim() === "") {
+			console.error("You have not selected a time");
+			return;
+		}
+
+		if (ticker.timerRunning() === true) {
+			ticker.stop();
+		} else {
+			ticker.start();
+		}
+	};
+
+	stopButton.onclick = function () {
+		if (ticker.timerRunning() === false) return;
+		ticker.stop();
+		displayTime.textContent = "0";
+		timeToCountDownFrom = 0;
+	};
+})();
