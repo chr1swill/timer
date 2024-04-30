0ae624c4e5e268660a7ec2dc5befbb8aa25fbeda
chr1swill <alexanderwilliams20003@gmail.com>
2024-04-29 21:54:12 -0400

    feat: make timer auto adj timer

--- worker.js
+++ worker.js
@@ -1,17 +1,30 @@
 let count = 0;
-const interval = 1000;
+let interval = 1000;
 let expected = Date.now();
 let totalDrift = 0;
 
 function timer() {
+	interval = 1000;
+
 	expected += interval;
 	console.log("expected time: ", expected);
+
 	const now = Date.now();
 	console.log("real time: ", now);
+
 	const drift = now - expected;
 	console.log("drift: ", drift);
+
 	totalDrift += drift;
+
+	if (now !== expected) {
+		interval = interval - drift;
+		totalDrift -= drift;
+	}
+	console.log("Current interval: ", interval);
+
 	console.log("accumlated drift: ", totalDrift);
+
 	count++;
 	postMessage(count);
 }
