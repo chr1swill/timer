16af3ca5484d9d2f28fe422a5312b6bf9e68aa6e
chr1swill <alexanderwilliams20003@gmail.com>
2024-05-06 00:20:18 -0400

    fix: make timer work properly - manage state of timer with module - add/remove comments - init expected time at the correct right before start timer

--- src/workers/timer.js
+++ src/workers/timer.js
@@ -6,17 +6,29 @@
 		#isRunning;
 
 		/**
+		 * @type{number}
+		 * This is only to be set by the start function
+		 * to ensure the drift is as accurate as possible
+		 *
+		 * The expected is the Date.now() right before the timer starts
+		 * allowing the timer to to have the best chance of having zero drift
+		 * or atleast an ammout of drift that is easily recoverable without
+		 * any timer deincrement skips
+		 */
+		#expected;
+
+		/**
 		 * @param{number} [startTime=0]
 		 */
 		constructor(startTime = 0) {
 			this.startTime = startTime;
 			this.interval = 1000;
-			this.expected = Date.now();
 			this.totalDrift = 0;
 			this.timer = this.timer.bind(this);
 			/**@type{number|null}*/
 			this.timeout = setTimeout(() => this.timer(), this.interval);
 			this.#isRunning = false;
+			this.#expected = 0;
 		}
 
 		isTicking() {
@@ -25,21 +37,21 @@
 
 		timer() {
 			if (this.startTime <= 0) {
-				this.stop();
+				this.reset();
 				console.log("Timer hit zero");
 				return;
 			}
 
-			this.expected += this.interval;
+			this.#expected += this.interval;
 			const now = Date.now();
-			console.log("Expected time: ", this.expected);
+			console.log("Expected time: ", this.#expected);
 			console.log("Time now:      ", now);
 
-			const drift = now - this.expected;
+			const drift = now - this.#expected;
 
 			this.totalDrift += drift;
 
-			if (now !== this.expected) {
+			if (now !== this.#expected) {
 				this.totalDrift -= drift;
 
 				if (this.timeout === null) {
@@ -87,19 +99,16 @@
 			this.#isRunning = true;
 			console.log("In start() this.#isRunning set to: ", this.#isRunning);
 
+			this.#expected = Date.now();
 			this.timeout = setTimeout(() => this.timer(), this.interval);
 			console.log("This is the START timeout id: ", this.timeout);
 		}
 
 		stop() {
 			console.log("In stop() this.#isRunning set to: ", this.#isRunning);
-			console.trace("Stack trace of this.#isRunning: ", this.#isRunning);
-			//if (this.isTicking() === false) {
-			//console.error("Timer is not running, cannot stop it");
-			//return;
-			//}
 
 			this.#isRunning = false;
+			this.#expected = 0;
 			if (this.timeout === null) {
 				console.error("There is no timeout to clear");
 				return;
@@ -119,80 +128,80 @@
 		}
 	}
 
-	/**
-	 * @param{number} minsInputValue
-	 * @param{number} secsInputValue
-	 * @returns{number} total number of seconds from inputs
-	 */
-	function normalizeInputedTimeToSeconds(minsInputValue, secsInputValue) {
-		return minsInputValue * 60 + secsInputValue;
-	}
-
-	/**
-	 * @typedef{import("../../types/types").MessageT} MessageT
-	 *
-	 * @typedef{import("../../types/types").StartMsg} StartMsg
-	 */
-	self.addEventListener("message", function (e) {
+	(function timerState() {
 		/**
-		 * @type{MessageT}
+		 * @param{number} minsInputValue
+		 * @param{number} secsInputValue
+		 * @returns{number} total number of seconds from inputs
 		 */
-		const data = e.data;
+		function normalizeInputedTimeToSeconds(minsInputValue, secsInputValue) {
+			return minsInputValue * 60 + secsInputValue;
+		}
+
 		const timer = new Timer();
 
 		/**
-		 * @link{import("../../types/types").CommandType}
-		 * Check that enum for better understand of the values of this switch
+		 * @typedef{import("../../types/types").MessageT} MessageT
+		 *
+		 * @typedef{import("../../types/types").StartMsg} StartMsg
 		 */
-		switch (data.command) {
-			case /**@type{import("../../types/types").CommandType.START} = 0 */ 0:
-				if (timer.isTicking() === true) {
-					console.warn(
-						"Timer is already running, stop the timer before starting a new one",
+		self.addEventListener("message", function (e) {
+			/**
+			 * @type{MessageT}
+			 */
+			const data = e.data;
+
+			/**
+			 * @link{import("../../types/types").CommandType}
+			 * Check that enum for better understand of the values of this switch
+			 */
+			switch (data.command) {
+				case /**@type{import("../../types/types").CommandType.START} = 0 */ 0:
+					if (timer.isTicking() === true) {
+						console.warn(
+							"Timer is already running, stop the timer before starting a new one",
+						);
+						return;
+					}
+
+					console.log("timer on timer: ", timer.startTime);
+					if (timer.startTime > 0) {
+						timer.start();
+						return;
+					}
+
+					const message = /**@type{StartMsg}*/ (data);
+					const totalSeconds = normalizeInputedTimeToSeconds(
+						message.options.minutes,
+						message.options.seconds,
 					);
-					return;
-				}
 
-				console.log("timer on timer: ", timer.startTime);
-				if (timer.startTime > 0) {
-					timer.start();
-					return;
-				}
+					if (timer.startTime === 0) {
+						timer.startTime = totalSeconds;
+						timer.start();
+					}
+					break;
+				case /**@type{import("../../types/types").CommandType.STOP} = 1*/ 1:
+					timer.stop();
+					break;
+				case /**@type{import("../../types/types").CommandType.RESET = 2}*/ 2:
+					timer.reset();
+					this.postMessage(0);
+					break;
+				default:
+					console.error(
+						"recieved message data of an unknown type",
+						data.command,
+					);
+			}
+		});
 
-				const message = /**@type{StartMsg}*/ (data);
-				const totalSeconds = normalizeInputedTimeToSeconds(
-					message.options.minutes,
-					message.options.seconds,
-				);
+		self.addEventListener("messageerror", function (e) {
+			console.error("Error: ", e, " Source: ", e.source);
+		});
 
-				if (timer.startTime === 0) {
-					timer.startTime = totalSeconds;
-					timer.start();
-				}
-				break;
-			case /**@type{import("../../types/types").CommandType.STOP} = 1*/ 1:
-				//if (timer.isTicking() === false) {
-				//console.warn(
-				//"Timer is already stopped, start a timer before attempting to stop it",
-				//);
-				//return;
-				//}
-
-				timer.stop();
-				break;
-			case /**@type{import("../../types/types").CommandType.RESET = 2}*/ 2:
-				timer.reset();
-				break;
-			default:
-				console.error("recieved message data of an unknown type", data.command);
-		}
-	});
-
-	self.addEventListener("messageerror", function (e) {
-		console.error("Error: ", e, " Source: ", e.source);
-	});
-
-	self.addEventListener("error", function (e) {
-		console.error("Error: ", e.error);
-	});
+		self.addEventListener("error", function (e) {
+			console.error("Error: ", e.error);
+		});
+	})();
 })();
